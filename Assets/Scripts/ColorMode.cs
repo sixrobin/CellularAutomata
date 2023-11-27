8b74b464109952c722b800139489fcfee9e04643
Robin Six <55784799+sixrobin@users.noreply.github.com>
2023-11-27 21:58:18 +0100

    ColorMode values summaries

--- Assets/Scripts/ColorMode.cs
+++ Assets/Scripts/ColorMode.cs
@@ -4,9 +4,24 @@ namespace CellularAutomata
     
     public enum ColorMode
     {
+        /// <summary>
+        /// White cells.
+        /// </summary>
         [InspectorName("None")]            NONE,
+        
+        /// <summary>
+        /// Use the cell current state to sample a color from a gradient.
+        /// </summary>
         [InspectorName("State")]           STATE,
+        
+        /// <summary>
+        /// Use the cell position XYZ as input for RGB channels.
+        /// </summary>
         [InspectorName("Position")]        POSITION,
+        
+        /// <summary>
+        /// Use the cell distance to automaton center to sample a color from a gradient.
+        /// </summary>
         [InspectorName("Center distance")] CENTER_DISTANCE,
     }
 }
